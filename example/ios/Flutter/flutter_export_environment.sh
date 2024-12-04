91bf50e00eb6ef2a101c505d351630403ab69c1c
Muhammad Saeed <saeed.bitsclans@gmail.com>
2024-12-04 17:43:36 +0500

    fixes

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Sdk-Setup\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Office Projects\Package Customization\agora_rtc_engine-5.3.1\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
